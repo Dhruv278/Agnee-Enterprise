63dcb73717a23e383b0fa41e7552ecddb5f0036b
Dhruv <dhruvgopani8@gmail.com>
2024-03-14 17:51:11 +0530

    add build1

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -13,6 +13,6 @@ let initialState={}
 
 
 const middleware=[thunk];
-const store=createStore(reducer,initialState,composeWithDevTools(applyMiddleware(...middleware) ))
+const store=createStore(reducer,initialState)
 
 export default store;
