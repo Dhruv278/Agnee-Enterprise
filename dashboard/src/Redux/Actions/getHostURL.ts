607342736a6ee26e0cefe0360debce8765aa2378
Dhruv Gopani <dhruvgopani8@gmail.com>
2024-08-25 15:17:16 +0530

    change ganerate excel file functionality build

--- dashboard/src/Redux/Actions/getHostURL.ts
+++ dashboard/src/Redux/Actions/getHostURL.ts
@@ -1,7 +1,6 @@
 export const getHostUrl = (): string => {
     const currentHost = window.location.host;
 const currentProtocol = window.location.protocol;
-    // return `${currentProtocol}//${currentHost}`
-    return "http://127.0.0.1:4000"
+    return `${currentProtocol}//${currentHost}`
 }
 
